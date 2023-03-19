cb5b060cb960ae16a24457c274c43fa99788c6f4
nishu kumar <kumarnishu437@gmail.com>
2023-03-19 13:27:14 +0530

    added firebase

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const path = require('path')
-
+const dotenv = require("dotenv");
+const PostRouter = require("./routes/posts")
 // express app
 const app = express()
 
@@ -9,28 +10,12 @@ app.set("views", path.join(__dirname, "templates"));
 app.set("view engine", "pug");
 
 // app middlewares
+dotenv.config()
 app.use(express.json())
 app.use(express.static('assets'))
 
 //app routes
-app.get("/detail", function (req, res) {
-    let youtube = `
-       <iframe width="560" height="315" src="https://www.youtube.com/embed/wrdt1nkcW2c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
-    let gist =` <script src="https://gist.github.com/kumarnishu/1acfc36f0d11b4ba47da1f50f810c249.js"></script>`
-    let context = {
-        title: "Blogger Post Detail Page",
-        youtube,
-        gist 
-    }
-    res.render("post-detail", context);
-});
-app.get("/", function (req, res) {
-    
-    let context = {
-        title: "Blogger Home Page"
-    }
-    res.render("home", context);
-});
+app.use("/", PostRouter)
 
 app.get("*", function (req, res, next) {
     res.status(200).send('Sorry, requested page not found.');
@@ -38,6 +23,6 @@ app.get("*", function (req, res, next) {
 });
 
 // server setup
-app.listen(8080, function () {
-    console.log(`http://localhost:8080`);
+app.listen(5000, function () {
+    console.log(`http://localhost:5000`);
 });
---
+++ config/firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+
+module.exports = auth
+module.exports = db
+module.exports = storage
